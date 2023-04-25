2a02a52947da97759e7ca1e48b7d09f6785c5a84
TinyTurtles <lyh1932573426@163.com>
2023-04-25 19:44:47 +0800

    20230425，更新算法

--- compression/burrows_wheeler.py
+++ compression/burrows_wheeler.py
@@ -1,78 +1,30 @@
 """
-https://en.wikipedia.org/wiki/Burrows%E2%80%93Wheeler_transform
-
-The Burrows–Wheeler transform (BWT, also called block-sorting compression)
-rearranges a character string into runs of similar characters. This is useful
-for compression, since it tends to be easy to compress a string that has runs
-of repeated characters by techniques such as move-to-front transform and
-run-length encoding. More importantly, the transformation is reversible,
-without needing to store any additional data except the position of the first
-original character. The BWT is thus a "free" method of improving the efficiency
-of text compression algorithms, costing only some extra computation.
+    BWT（Burrows-Wheeler Transform）算法是一种数据压缩算法，
+    通过对数据进行变换，使得数据中的重复模式得到更好的压缩。
+    BWT算法的基本思想是将原始数据转换为一种更易于压缩的形式，
+    然后再使用其他压缩算法进行压缩。
 """
 from __future__ import annotations
 
-from typing import TypedDict
 
-
-class BWTTransformDict(TypedDict):
+class BWTTransformDict:
     bwt_string: str
     idx_original_string: int
 
 
-def all_rotations(s: str) -> list[str]:
-    """
-    :param s: The string that will be rotated len(s) times.
-    :return: A list with the rotations.
-    :raises TypeError: If s is not an instance of str.
-    Examples:
-
-    >>> all_rotations("^BANANA|") # doctest: +NORMALIZE_WHITESPACE
-    ['^BANANA|', 'BANANA|^', 'ANANA|^B', 'NANA|^BA', 'ANA|^BAN', 'NA|^BANA',
-    'A|^BANAN', '|^BANANA']
-    >>> all_rotations("a_asa_da_casa") # doctest: +NORMALIZE_WHITESPACE
-    ['a_asa_da_casa', '_asa_da_casaa', 'asa_da_casaa_', 'sa_da_casaa_a',
-    'a_da_casaa_as', '_da_casaa_asa', 'da_casaa_asa_', 'a_casaa_asa_d',
-    '_casaa_asa_da', 'casaa_asa_da_', 'asaa_asa_da_c', 'saa_asa_da_ca',
-    'aa_asa_da_cas']
-    >>> all_rotations("panamabanana") # doctest: +NORMALIZE_WHITESPACE
-    ['panamabanana', 'anamabananap', 'namabananapa', 'amabananapan',
-    'mabananapana', 'abananapanam', 'bananapanama', 'ananapanamab',
-    'nanapanamaba', 'anapanamaban', 'napanamabana', 'apanamabanan']
-    >>> all_rotations(5)
-    Traceback (most recent call last):
-        ...
-    TypeError: The parameter s type must be str.
-    """
-    if not isinstance(s, str):
-        raise TypeError("The parameter s type must be str.")
-
-    return [s[i:] + s[:i] for i in range(len(s))]
-
-
 def bwt_transform(s: str) -> BWTTransformDict:
-    """
-    :param s: The string that will be used at bwt algorithm
-    :return: the string composed of the last char of each row of the ordered
-    rotations and the index of the original string at ordered rotations list
-    :raises TypeError: If the s parameter type is not str
-    :raises ValueError: If the s parameter is empty
-    Examples:
+    """ 将字符串采用BWT算法进行变换
+        输入值：
+            s：需要变换的字符串
 
-    >>> bwt_transform("^BANANA")
-    {'bwt_string': 'BNN^AAA', 'idx_original_string': 6}
-    >>> bwt_transform("a_asa_da_casa")
-    {'bwt_string': 'aaaadss_c__aa', 'idx_original_string': 3}
-    >>> bwt_transform("panamabanana")
-    {'bwt_string': 'mnpbnnaaaaaa', 'idx_original_string': 11}
-    >>> bwt_transform(4)
-    Traceback (most recent call last):
-        ...
-    TypeError: The parameter s type must be str.
-    >>> bwt_transform('')
-    Traceback (most recent call last):
-        ...
-    ValueError: The parameter s must not be empty.
+        返回值：
+            返回BWTTransformDict类
+            BWTTransformDict[bwt_string]: 经过BWT算法处理的字符串
+            BWTTransformDict[idx_original_string]: 列表索引
+
+        示例：
+            bwt_transform("a_asa_da_casa")
+            result: {'bwt_string': 'aaaadss_c__aa', 'idx_original_string': 3}
     """
     if not isinstance(s, str):
         raise TypeError("The parameter s type must be str.")
@@ -90,50 +42,17 @@ def bwt_transform(s: str) -> BWTTransformDict:
 
 
 def reverse_bwt(bwt_string: str, idx_original_string: int) -> str:
-    """
-    :param bwt_string: The string returned from bwt algorithm execution
-    :param idx_original_string: A 0-based index of the string that was used to
-    generate bwt_string at ordered rotations list
-    :return: The string used to generate bwt_string when bwt was executed
-    :raises TypeError: If the bwt_string parameter type is not str
-    :raises ValueError: If the bwt_string parameter is empty
-    :raises TypeError: If the idx_original_string type is not int or if not
-    possible to cast it to int
-    :raises ValueError: If the idx_original_string value is lower than 0 or
-    greater than len(bwt_string) - 1
+    """ 将经BWT算法变换的字符串还原
+        输入值：
+            BWTTransformDict[bwt_string]: 经过BWT算法变换的字符串
+            BWTTransformDict[idx_original_string]: 列表索引
 
-    >>> reverse_bwt("BNN^AAA", 6)
-    '^BANANA'
-    >>> reverse_bwt("aaaadss_c__aa", 3)
-    'a_asa_da_casa'
-    >>> reverse_bwt("mnpbnnaaaaaa", 11)
-    'panamabanana'
-    >>> reverse_bwt(4, 11)
-    Traceback (most recent call last):
-        ...
-    TypeError: The parameter bwt_string type must be str.
-    >>> reverse_bwt("", 11)
-    Traceback (most recent call last):
-        ...
-    ValueError: The parameter bwt_string must not be empty.
-    >>> reverse_bwt("mnpbnnaaaaaa", "asd") # doctest: +NORMALIZE_WHITESPACE
-    Traceback (most recent call last):
-        ...
-    TypeError: The parameter idx_original_string type must be int or passive
-    of cast to int.
-    >>> reverse_bwt("mnpbnnaaaaaa", -1)
-    Traceback (most recent call last):
-        ...
-    ValueError: The parameter idx_original_string must not be lower than 0.
-    >>> reverse_bwt("mnpbnnaaaaaa", 12) # doctest: +NORMALIZE_WHITESPACE
-    Traceback (most recent call last):
-        ...
-    ValueError: The parameter idx_original_string must be lower than
-    len(bwt_string).
-    >>> reverse_bwt("mnpbnnaaaaaa", 11.0)
-    'panamabanana'
-    >>> reverse_bwt("mnpbnnaaaaaa", 11.4)
-    'panamabanana'
+        返回值：
+            返回还原后的字符串
+
+        示例：
+            reverse_bwt("aaaadss_c__aa", 3)
+            result: 'a_asa_da_casa'
     """
     if not isinstance(bwt_string, str):
         raise TypeError("The parameter bwt_string type must be str.")
@@ -161,16 +80,17 @@ def reverse_bwt(bwt_string: str, idx_original_string: int) -> str:
     return ordered_rotations[idx_original_string]
 
 
+def all_rotations(s: str) -> list[str]:
+    if not isinstance(s, str):
+        raise TypeError("The parameter s type must be str.")
+
+    return [s[i:] + s[:i] for i in range(len(s))]
+
+
 if __name__ == "__main__":
-    entry_msg = "Provide a string that I will generate its BWT transform: "
-    s = input(entry_msg).strip()
-    result = bwt_transform(s)
-    print(
-        f"Burrows Wheeler transform for string '{s}' results "
-        f"in '{result['bwt_string']}'"
-    )
-    original_string = reverse_bwt(result["bwt_string"], result["idx_original_string"])
-    print(
-        f"Reversing Burrows Wheeler transform for entry '{result['bwt_string']}' "
-        f"we get original string '{original_string}'"
-    )
+    entry_msg = "a_asa_da_casa"
+    s = entry_msg.strip()
+    bwt_result = bwt_transform(s)
+    print(bwt_result)
+    original_string = reverse_bwt(bwt_result["bwt_string"], bwt_result["idx_original_string"])
+    print(original_string)
--- compression/huffman.py
+++ compression/huffman.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
-
-import sys
+from burrows_wheeler import bwt_transform, reverse_bwt
 
 
 class Letter:
@@ -20,26 +19,132 @@ class TreeNode:
         self.right: Letter | TreeNode = right
 
 
-def parse_file(file_path: str) -> list[Letter]:
+class HuffmanDict:
+    Huf_encode: list
+    Huf_letters: list
+
+
+def huffman_encode(raw_file_list: list) -> HuffmanDict:
+    """ 哈夫曼编码压缩文件
+        输入值：
+            raw_file_list: 字符串列表
+
+        返回值：
+            返回HuffmanDict类
+            HuffmanDict[Huf_encode]: 经过huffman编码处理的字符串
+            HuffmanDict[Huf_letters]: huffman编码表
+
+        示例：
+            # raw_file_list = ["a", "a", "a", "a", "d", "s", "s", "_", "c", "_", "_", "a", "a"]
+            # Huf_encode_result = huffman_encode(raw_file_list)
+            # result:
+                Huf_encode_result["Huf_encode"]:
+                    ['0', '0', '0', '0', '1110', '110', '110', '10', '1111', '10', '10', '0', '0']
+                Huf_encode_result["Huf_letters"]:
+                    {'a': '0', '_': '10', 's': '110', 'd': '1110', 'c': '1111'}
     """
-    Read the file and build a dict of all letters and their
-    frequencies, then convert the dict into a list of Letters.
+    letters_list = parse_file(raw_file_list)
+    root = build_tree(letters_list)
+    letters = {
+        k: v for letter in traverse_tree(root, "") for k, v in letter.bitstring.items()
+    }
+    encode_list = []
+    for c in raw_file_list:
+        encode_list.append(letters[c])
+
+    response: HuffmanDict = {"Huf_encode": encode_list, "Huf_letters": letters}
+    return response
+
+
+def huffman_decode(huf_file: HuffmanDict) -> list:
+    """ 哈夫曼编码解压缩文件
+        输入值：
+            huf_file: HuffmanDict类
+                HuffmanDict[Huf_encode]: 经过huffman编码处理的字符串
+                HuffmanDict[Huf_letters]: huffman编码表
+
+        返回值：
+            返回字符串列表
+
+        示例：
+            # Huf_decode_result = huffman_decode(Huf_encode_result)
+            # result: 'aaaadss_c__aa'
     """
+    result = []
+    for c in huf_file["Huf_encode"]:
+        result.append(return_key(huf_file["Huf_letters"], c))
+    return result
+
+
+def bwt_huffman_encode(raw_file_list: list) -> HuffmanDict:
+    """ BWT和哈夫曼编码压缩文件
+        输入值：
+            raw_file_list: 字符串列表
+
+        返回值：
+            返回HuffmanDict类
+            HuffmanDict[Huf_encode]: 经过huffman编码处理的字符串
+            HuffmanDict[Huf_letters]: huffman编码表
+            HuffmanDict[Huf_letters]: bwt列表索引表
+
+        示例：
+            # raw_file_list = ["a", "a", "a", "a", "d", "s", "s", "_", "c", "_", "_", "a", "a"]
+            # Bwt_Huf_encode_result = bwt_huffman_encode(raw_file_list)
+            # result:
+                Bwt_Huf_encode_result["Huf_encode"]:
+                    ['1110', '10', '110', '10', '0', '0', '0', '0', '0', '10', '0', '110', '1111']
+                Bwt_Huf_encode_result["Huf_letters"]:
+                    {'a': '0', '_': '10', 's': '110', 'c': '1110', 'd': '1111'}
+                Bwt_Huf_encode_result["idx_original_string"]:
+                    5
+    """
+    bwt_result = bwt_transform(''.join(raw_file_list))
+    temp=[]
+    for ch in bwt_result["bwt_string"]:
+        temp.append(ch)
+    bwt_huffman_result = huffman_encode(temp)
+    bwt_huffman_result["idx_original_string"] = bwt_result["idx_original_string"]
+    return bwt_huffman_result
+
+
+def bwt_huffman_decode(huf_file: HuffmanDict) -> str:
+    """ BWT和哈夫曼编码解压缩文件
+        输入值：
+        返回HuffmanDict类
+            HuffmanDict[Huf_encode]: 经过huffman编码处理的字符串
+            HuffmanDict[Huf_letters]: huffman编码表
+            HuffmanDict[Huf_letters]: bwt列表索引表
+
+        返回值：
+            返回字符串
+
+        示例：
+            # Bwt_Huf_decode_result = bwt_huffman_decode(Bwt_Huf_encode_result)
+            # result: 'aaaadss_c__aa'
+    """
+    huf_decode = huffman_decode(huf_file)
+    bwt_huffman_result = reverse_bwt("".join(huf_decode), huf_file["idx_original_string"])
+
+    return bwt_huffman_result
+
+
+def return_key(huf_letters, item):
+    for key, value in huf_letters.items():
+        if value == item:
+            return key
+    return "Key Not Found"
+
+
+def parse_file(raw_file_list: list) -> list[Letter]:
+    # 阅读文件，构建一个包含所有字母及其频率的dict，然后将dict转换为字母列表。
     chars: dict[str, int] = {}
-    with open(file_path) as f:
-        while True:
-            c = f.read(1)
-            if not c:
-                break
-            chars[c] = chars[c] + 1 if c in chars else 1
+    for c in raw_file_list:
+        chars[c] = chars[c] + 1 if c in chars else 1
     return sorted((Letter(c, f) for c, f in chars.items()), key=lambda x: x.freq)
 
 
 def build_tree(letters: list[Letter]) -> Letter | TreeNode:
-    """
-    Run through the list of Letters and build the min heap
-    for the Huffman Tree.
-    """
+    # 运行字母列表，并为霍夫曼树构建最小堆。
     response: list[Letter | TreeNode] = letters  # type: ignore
     while len(response) > 1:
         left = response.pop(0)
@@ -52,10 +157,7 @@ def build_tree(letters: list[Letter]) -> Letter | TreeNode:
 
 
 def traverse_tree(root: Letter | TreeNode, bitstring: str) -> list[Letter]:
-    """
-    Recursively traverse the Huffman Tree to set each
-    Letter's bitstring dictionary, and return the list of Letters
-    """
+    # 递归遍历霍夫曼树以设置每个字母的位串字典，并返回字母列表
     if isinstance(root, Letter):
         root.bitstring[root.letter] = bitstring
         return [root]
@@ -66,27 +168,32 @@ def traverse_tree(root: Letter | TreeNode, bitstring: str) -> list[Letter]:
     return letters
 
 
-def huffman(file_path: str) -> None:
-    """
-    Parse the file, build the tree, then run through the file
-    again, using the letters dictionary to find and print out the
-    bitstring for each letter.
-    """
-    letters_list = parse_file(file_path)
-    root = build_tree(letters_list)
-    letters = {
-        k: v for letter in traverse_tree(root, "") for k, v in letter.bitstring.items()
-    }
-    print(f"Huffman Coding  of {file_path}: ")
+if __name__ == "__main__":
+    file_path = "./README.md"
+    raw_file_list = []
     with open(file_path) as f:
         while True:
             c = f.read(1)
             if not c:
                 break
-            print(letters[c], end=" ")
-    print()
+            raw_file_list.append(c)
 
+    raw_file_list = ["a", "a", "a", "a", "d", "s", "s", "_", "c", "_", "_", "a", "a"]
+    # Huffman编码压缩
+    Huf_encode_result = huffman_encode(raw_file_list)
+    print("huffman编码后: \n", Huf_encode_result["Huf_encode"])
+    print("huffman编码索引表: \n", Huf_encode_result["Huf_letters"])
 
-if __name__ == "__main__":
-    # pass the file path to the huffman function
-    huffman(sys.argv[1])
+    # Huffman编码解压缩
+    Huf_decode_result = huffman_decode(Huf_encode_result)
+    print("huffman解码后: \n", "".join(Huf_decode_result))
+
+    # BWT和Huffman编码后压缩
+    Bwt_Huf_encode_result = bwt_huffman_encode(raw_file_list)
+    print("bwt_huffman编码后: \n", Bwt_Huf_encode_result["Huf_encode"])
+    print("bwt_huffman编码索引表: \n", Bwt_Huf_encode_result["Huf_letters"])
+    print("bwt_列表索引表: \n", Bwt_Huf_encode_result["idx_original_string"])
+
+    # BWT和Huffman编码后解压缩
+    Bwt_Huf_decode_result = bwt_huffman_decode(Bwt_Huf_encode_result)
+    print("bwt_huffman解码后: \n", Bwt_Huf_decode_result)
